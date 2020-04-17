255d868887644dd524df07b5b7db2b2bb613f3f4
Kyle Ratti <kyleratti@users.noreply.github.com>
2020-04-17 17:08:52 -0400

    Cleaned up imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,6 @@
 import * as React from "react";
-import { render, hydrate } from "react-dom";
-import { renderToString } from "react-dom/server";
+import { render } from "react-dom";
 import "./css/app.scss";
 import Main from "./Main";
-import LandingPage from "./pages/LandingPage";
 
 render(<Main />, document.getElementById("main"));
