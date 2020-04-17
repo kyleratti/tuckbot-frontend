ce9655b35a8e886c28692549fe1b8d6573cc8d29
Kyle Ratti <kyleratti@users.noreply.github.com>
2020-04-17 17:01:13 -0400

    Removed hydrate check

--- src/App.tsx
+++ src/App.tsx
@@ -5,10 +5,4 @@ import "./css/app.scss";
 import Main from "./Main";
 import LandingPage from "./pages/LandingPage";
 
-const mainElement = document.getElementById("main");
-
-if (mainElement?.hasChildNodes()) {
-  hydrate(<Main />, mainElement);
-} else {
-  render(<Main />, mainElement);
-}
+render(<Main />, document.getElementById("main"));
