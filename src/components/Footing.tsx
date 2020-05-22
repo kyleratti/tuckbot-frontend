d0fd2113ffd9b3f9837aac5fa3c9f8c5c36400c0
Kyle <kyleratti@users.noreply.github.com>
2020-05-22 16:57:29 -0400

    Removed random video link
    This feature ends up costing money in the long run as it increases the chance a random, otherwise totally unwatched video will not be pruned

--- src/components/Footing.tsx
+++ src/components/Footing.tsx
@@ -1,15 +1,11 @@
 import * as React from "react";
 import ContactLink from "./Links/ContactLink";
-import RandomVideoLink from "./Links/RandomVideoLink";
 
 const Footing: React.FunctionComponent = () => (
   <footer>
     <ul className="inline-list">
       <li>
         <ContactLink />
-      </li>
-      <li>
-        <RandomVideoLink />
       </li>
     </ul>
   </footer>
