fe2c6476cbc74c9832119fd6f2a7f36b1d5ada78
Kyle <kyleratti@users.noreply.github.com>
2020-05-31 09:43:06 -0400

    Update contact page

--- src/pages/ContactPage/ContactPage.tsx
+++ src/pages/ContactPage/ContactPage.tsx
@@ -3,13 +3,15 @@ import PageContainer from "../../components/PageContainer";
 
 const ContactPage: React.FunctionComponent = () => (
   <PageContainer>
-    <div>
+    <div className="contactPage">
       <p>
-        <span className="bold">Please note: </span>None of the content on this
-        page is owned by the operators of this website.
+        <b>Please note:</b> None of the content on this page is owned by the
+        operators of this website.
       </p>
 
-      <p>You are free to share links to this website.</p>
+      <p>
+        You are free to re-post or share links to any pages on this website.
+      </p>
 
       <dl className="inline-list">
         <dt>General Inquiries</dt>
