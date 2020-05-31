388c5ba8fd6d2935c111a77584b8a9619390ca1c
Kyle <kyleratti@users.noreply.github.com>
2020-05-31 10:14:38 -0400

    Fix Tucker image gallery

--- src/pages/TuckerPage/components/photos.ts
+++ src/pages/TuckerPage/components/photos.ts
@@ -1,60 +1,20 @@
-interface PhotoType {
-  original: string;
-  thumbnail: string;
-  description?: string;
-}
+const photo = (str: string) => ({
+  original: require(`@/img/${str}`).default,
+  thumbnail: require(`@/img/thumb.${str}`).default,
+});
 
 export const photos = [
-  {
-    original: require("@/img/tuck-bowtie.jpg"),
-    thumbnail: require("@/img/thumb.tuck-bowtie.jpg"),
-  },
-  {
-    original: require("@/img/tuck-couch.jpg"),
-    thumbnail: require("@/img/thumb.tuck-couch.jpg"),
-  },
-  {
-    original: require("@/img/tuck-cozy.jpg"),
-    thumbnail: require("@/img/thumb.tuck-cozy.jpg"),
-  },
-  {
-    original: require("@/img/tuck-deck.jpg"),
-    thumbnail: require("@/img/thumb.tuck-deck.jpg"),
-  },
-  {
-    original: require("@/img/tuck-ducksuck.jpg"),
-    thumbnail: require("@/img/thumb.tuck-ducksuck.jpg"),
-  },
-  {
-    original: require("@/img/tuck-fiata-wind.jpg"),
-    thumbnail: require("@/img/thumb.tuck-fiata-wind.jpg"),
-  },
-  {
-    original: require("@/img/tuck-fiata.jpg"),
-    thumbnail: require("@/img/thumb.tuck-fiata.jpg"),
-  },
-  {
-    original: require("@/img/tuck-lazy.jpg"),
-    thumbnail: require("@/img/thumb.tuck-lazy.jpg"),
-  },
-  {
-    original: require("@/img/tuck-noselick.jpg"),
-    thumbnail: require("@/img/thumb.tuck-noselick.jpg"),
-  },
-  {
-    original: require("@/img/tuck-rainy.jpg"),
-    thumbnail: require("@/img/thumb.tuck-rainy.jpg"),
-  },
-  {
-    original: require("@/img/tuck-stump.jpg"),
-    thumbnail: require("@/img/thumb.tuck-stump.jpg"),
-  },
-  {
-    original: require("@/img/tuck-yawn.jpg"),
-    thumbnail: require("@/img/thumb.tuck-yawn.jpg"),
-  },
-  {
-    original: require("@/img/tuck-yawn2.jpg"),
-    thumbnail: require("@/img/thumb.tuck-yawn2.jpg"),
-  },
-];
+  "tuck-bowtie.jpg",
+  "tuck-couch.jpg",
+  "tuck-cozy.jpg",
+  "tuck-deck.jpg",
+  "tuck-ducksuck.jpg",
+  "tuck-fiata-wind.jpg",
+  "tuck-fiata.jpg",
+  "tuck-lazy.jpg",
+  "tuck-noselick.jpg",
+  "tuck-rainy.jpg",
+  "tuck-stump.jpg",
+  "tuck-yawn.jpg",
+  "tuck-yawn2.jpg",
+].map((str) => photo(str));
