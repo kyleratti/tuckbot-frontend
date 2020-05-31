c0de653a6b861403df8cce276c45d638f8fc7328
Kyle <kyleratti@users.noreply.github.com>
2020-05-31 09:48:39 -0400

    Update syntax of photos.ts

--- src/pages/TuckerPage/components/photos.ts
+++ src/pages/TuckerPage/components/photos.ts
@@ -7,54 +7,54 @@ interface PhotoType {
 export const photos = [
   {
     original: require("@/img/tuck-bowtie.jpg"),
-    thumbnail: require("@/img/thumb.tuck-bowtie.jpg")
+    thumbnail: require("@/img/thumb.tuck-bowtie.jpg"),
   },
   {
     original: require("@/img/tuck-couch.jpg"),
-    thumbnail: require("@/img/thumb.tuck-couch.jpg")
+    thumbnail: require("@/img/thumb.tuck-couch.jpg"),
   },
   {
     original: require("@/img/tuck-cozy.jpg"),
-    thumbnail: require("@/img/thumb.tuck-cozy.jpg")
+    thumbnail: require("@/img/thumb.tuck-cozy.jpg"),
   },
   {
     original: require("@/img/tuck-deck.jpg"),
-    thumbnail: require("@/img/thumb.tuck-deck.jpg")
+    thumbnail: require("@/img/thumb.tuck-deck.jpg"),
   },
   {
     original: require("@/img/tuck-ducksuck.jpg"),
-    thumbnail: require("@/img/thumb.tuck-ducksuck.jpg")
+    thumbnail: require("@/img/thumb.tuck-ducksuck.jpg"),
   },
   {
     original: require("@/img/tuck-fiata-wind.jpg"),
-    thumbnail: require("@/img/thumb.tuck-fiata-wind.jpg")
+    thumbnail: require("@/img/thumb.tuck-fiata-wind.jpg"),
   },
   {
     original: require("@/img/tuck-fiata.jpg"),
-    thumbnail: require("@/img/thumb.tuck-fiata.jpg")
+    thumbnail: require("@/img/thumb.tuck-fiata.jpg"),
   },
   {
     original: require("@/img/tuck-lazy.jpg"),
-    thumbnail: require("@/img/thumb.tuck-lazy.jpg")
+    thumbnail: require("@/img/thumb.tuck-lazy.jpg"),
   },
   {
     original: require("@/img/tuck-noselick.jpg"),
-    thumbnail: require("@/img/thumb.tuck-noselick.jpg")
+    thumbnail: require("@/img/thumb.tuck-noselick.jpg"),
   },
   {
     original: require("@/img/tuck-rainy.jpg"),
-    thumbnail: require("@/img/thumb.tuck-rainy.jpg")
+    thumbnail: require("@/img/thumb.tuck-rainy.jpg"),
   },
   {
     original: require("@/img/tuck-stump.jpg"),
-    thumbnail: require("@/img/thumb.tuck-stump.jpg")
+    thumbnail: require("@/img/thumb.tuck-stump.jpg"),
   },
   {
     original: require("@/img/tuck-yawn.jpg"),
-    thumbnail: require("@/img/thumb.tuck-yawn.jpg")
+    thumbnail: require("@/img/thumb.tuck-yawn.jpg"),
   },
   {
     original: require("@/img/tuck-yawn2.jpg"),
-    thumbnail: require("@/img/thumb.tuck-yawn2.jpg")
-  }
+    thumbnail: require("@/img/thumb.tuck-yawn2.jpg"),
+  },
 ];
