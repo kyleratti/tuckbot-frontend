a5f9d4c0dd30261a90e5ba8e7adf7bdb2cae23dd
Kyle <kyleratti@users.noreply.github.com>
2020-07-10 15:19:30 -0400

    Add react-ga module

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,15 @@
 import * as React from "react";
 import { render } from "react-dom";
+import ReactGA from "react-ga";
 import "./css/app.scss";
 import Main from "./Main";
+
+const googleAnalyticsId = process.env.GOOGLE_ANALYTICS_TRACKING_ID;
+
+if (googleAnalyticsId) {
+  console.log(googleAnalyticsId);
+  ReactGA.initialize(googleAnalyticsId);
+  ReactGA.pageview(window.location.pathname + window.location.hash);
+}
 
 render(<Main />, document.getElementById("main"));
