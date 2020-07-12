1344b5458d14c1b7d30189ef036ea55366465806
Kyle <kyleratti@users.noreply.github.com>
2020-07-11 21:45:53 -0400

    Fix petting Tucker on Tucker's page causing the heading to disappear

--- src/components/Heading.tsx
+++ src/components/Heading.tsx
@@ -1,29 +1,38 @@
 import React, { useState } from "react";
 import { Redirect } from "react-router";
+import ContactLink from "./Links/ContactLink";
 
 const TUCKER_PETS = 2;
 
 const Heading: React.FC = () => {
   const [pets, setPets] = useState(0);
 
-  if (pets >= TUCKER_PETS) return <Redirect push to="/tucker" />;
-
   return (
-    <header>
-      <div className="siteTitle">
-        <h1>
-          <span id="tuckie-boy" onClick={() => setPets(pets + 1)}>
-            🐶
+    <React.Fragment>
+      {pets >= TUCKER_PETS && <Redirect push to="/tucker" />}
+      <header>
+        <div className="siteTitle">
+          <h1>
+            <span id="tuckie-boy" onClick={() => setPets(pets + 1)}>
+              🐶
+            </span>
+            Tuckbot<span className="dot-tv">.tv</span>
+          </h1>
+        </div>
+        <div className="siteComment">
+          <span className="label">
+            #Black<span className="alt">Lives</span>Matter
           </span>
-          Tuckbot<span className="dot-tv">.tv</span>
-        </h1>
-      </div>
-      <div className="siteComment">
-        <span className="label">
-          #Black<span className="alt">Lives</span>Matter
-        </span>
-      </div>
-    </header>
+        </div>
+        <nav>
+          <ul>
+            <li>
+              <ContactLink />
+            </li>
+          </ul>
+        </nav>
+      </header>
+    </React.Fragment>
   );
 };
 
