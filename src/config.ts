76bae47a74d8096141a002b739030ca6b9d3212e
Kyle <kyleratti@users.noreply.github.com>
2020-08-12 19:50:02 -0400

    chore(docs): add TS Doc strings to config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,11 +1,26 @@
-import { configurator } from "@kyleratti/configurator";
+import configurator from "@kyleratti/configurator";
 
 type AppConfig = {
   tuckbot: {
+    /**
+     * tuckbot-api configuration
+     */
     api: {
+      /**
+       * The full URL to the root of the API server with no trailing '/'
+       * @example https://api.tuckbot.tv
+       */
       url: string;
     };
+
+    /**
+     * tuckbot-frontend configuration
+     */
     frontend: {
+      /**
+       * The Google Analytics tracking ID
+       * If not set, Google Analytics will not be included on the page build
+       */
       googleAnalyticsId: string;
     };
   };
