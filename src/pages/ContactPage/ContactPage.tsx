9c3d922be975e6bfb2a0618c9cc38071be784756
Kyle <kyleratti@users.noreply.github.com>
2020-08-25 22:10:29 -0400

    fix(ContactPage): add title

--- src/pages/ContactPage/ContactPage.tsx
+++ src/pages/ContactPage/ContactPage.tsx
@@ -1,32 +1,38 @@
 import * as React from "react";
 import PageContainer from "../../components/PageContainer";
+import { setTitle } from "../../services/title/hooks";
 
-const ContactPage: React.FC = () => (
-  <PageContainer>
-    <div className="contactPage">
-      <p>
-        <b>Please note:</b> None of the content on any pages of this site is
-        owned by the operators of this website. All content is provided as an
-        alternative courtesy service to submissions on Reddit.
-      </p>
+const ContactPage: React.FC = () => {
+  setTitle("Contact");
 
-      <p>
-        You are free to re-post or share links to any pages on this website.
-      </p>
+  return (
+    <PageContainer>
+      <div className="contactPage">
+        <p>
+          <b>Please note:</b> None of the content on any pages of this site is
+          owned by the operators of this website. All content is provided as an
+          alternative courtesy service to submissions on Reddit.
+        </p>
 
-      <p>Please note we are unable to locate any videos for you.</p>
+        <p>
+          You are free to re-post or share links to any pages on this website.
+        </p>
 
-      <dl className="inline-list">
-        <dt>General Inquiries</dt>
-        <dd>
-          <a href="mailto:inquire@tuckbot.tv">inquire@tuckbot.tv</a>
-        </dd>
-        <dt>Takedown Requests</dt>
-        <dd>
-          <a href="mailto:takedown@tuckbot.tv">takedown@tuckbot.tv</a>
-        </dd>
-      </dl>
-    </div>
-  </PageContainer>
-);
+        <p>Please note we are unable to locate any videos for you.</p>
+
+        <dl className="inline-list">
+          <dt>General Inquiries</dt>
+          <dd>
+            <a href="mailto:inquire@tuckbot.tv">inquire@tuckbot.tv</a>
+          </dd>
+          <dt>Takedown Requests</dt>
+          <dd>
+            <a href="mailto:takedown@tuckbot.tv">takedown@tuckbot.tv</a>
+          </dd>
+        </dl>
+      </div>
+    </PageContainer>
+  );
+};
+
 export default ContactPage;
