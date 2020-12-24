4d14e6956384d4c85220d90bb34ae2e697a252f6
Kyle Ratti <kyleratti@users.noreply.github.com>
2020-12-24 18:44:47 -0500

    fix: update page path with hash URLs

--- src/routes/helpers.ts
+++ src/routes/helpers.ts
@@ -3,4 +3,9 @@
  * @param str The hash URL to redirect to
  * @example setNewHashLocation("maintenance") // navigate to "#/maintenance"
  */
-export const setNewHashLocation = (str: string) => (location.hash = `#/${str}`);
+export const setNewHashLocation = (str: string) => {
+  location.hash = `#/${str}`;
+
+  // @ts-expect-error
+  setGAPAgeUrl(`/${str}`);
+};
